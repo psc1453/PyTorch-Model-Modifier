7309f0f7a7f8109615e80a8e4c774a5892711f5e
pscpeng <pscpeng@outlook.com>
2023-07-26 16:17:29 +0800

    Add quantization by channel

--- ModelModifier/tools/quantization/utils.py
+++ ModelModifier/tools/quantization/utils.py
@@ -30,15 +30,38 @@ def quantize_tensor_with_original_scale(tensor_input: torch.Tensor, width: int) 
         return quantized_tensor
 
 
+def quantize_weight_by_channel_with_original_scale(tensor_input: torch.Tensor, width: int) -> torch.Tensor:
+    assert isinstance(tensor_input, torch.Tensor)
+    assert isinstance(width, int)
+    assert width > 0
+
+    input_shape = tensor_input.shape
+    assert len(input_shape) == 4
+
+    kernel_num = input_shape[0]
+
+    quantized_tensor = torch.zeros_like(tensor_input)
+    for kernel_index in range(kernel_num):
+        quantized_tensor[kernel_index] = quantize_tensor_with_original_scale(tensor_input[kernel_index], width=width)
+
+    return quantized_tensor
+
+
 def quantize_model_parameters_with_original_scale(model_input: nn.Module, weight_width: int,
-                                                  bias_width: int) -> NNModule:
+                                                  bias_width: int, by_channel=False) -> NNModule:
     model = copy.deepcopy(model_input)
     model_parameters = model.state_dict()
     for parameter_name in model_parameters:
+        # TODO: Add support for layers without the name of conv and linear
         if 'weight' in parameter_name:
-            model_parameters[parameter_name] = quantize_tensor_with_original_scale(
-                tensor_input=model_parameters[parameter_name], width=weight_width
-            )
+            if by_channel:
+                model_parameters[parameter_name] = quantize_weight_by_channel_with_original_scale(
+                    tensor_input=model_parameters[parameter_name], width=weight_width
+                )
+            else:
+                model_parameters[parameter_name] = quantize_tensor_with_original_scale(
+                    tensor_input=model_parameters[parameter_name], width=weight_width
+                )
         elif 'bias' in parameter_name:
             model_parameters[parameter_name] = quantize_tensor_with_original_scale(
                 tensor_input=model_parameters[parameter_name], width=bias_width
