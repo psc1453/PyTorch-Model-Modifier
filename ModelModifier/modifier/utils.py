ea4ec5fe7fda3beab7b8a780b981b3769e6827e7
pscpeng <pscpeng@outlook.com>
2023-08-14 18:54:58 +0800

    Remove unnecessarry logic

--- ModelModifier/modifier/utils.py
+++ ModelModifier/modifier/utils.py
@@ -72,30 +72,23 @@ def insert_after(model_input: NNModule, insert_mapping: NodeInsertMapping) -> to
     symbolic_traced_module_dict = dict(symbolic_traced_module.named_modules())
     symbolic_traced_module_graph = symbolic_traced_module.graph
 
-    latest_node_is_new_inserted = False
     for current_node in symbolic_traced_module_graph.nodes:
-        # Skip an iteration if the last iteration inserts a new node, because this iteration is the new node
-        if latest_node_is_new_inserted:
-            # Next iteration will not enter this branch, it will be the originally existed node
-            latest_node_is_new_inserted = False
-        # Only originally existed node can Enter this branch.
-        else:
-            insert_config = get_insert_config(current_node, symbolic_traced_module_dict, insert_mapping)
-            # If this node match the patter, a new node needs to be inserted after it
-            if insert_config.should_insert:
-                # Get the next original node
-                next_origin_node = current_node.next
-                # Create temporary pointer for inserting
-                with symbolic_traced_module_graph.inserting_after(current_node):
-                    # Create new node after current node
-                    new_node = symbolic_traced_module_graph.call_function(insert_config.function_package.function,
-                                                                          kwargs=insert_config.function_package.parameter_dict)
-                    # Set the input of the new node to the output of the current node
-                    set_node_input(new_node, get_node_output(current_node))
-                    # Get the output of the new node
-                    new_node_output = get_node_output(new_node)
-                    # Link the output of the new node to the input of the next original node
-                    set_node_input(next_origin_node, new_node_output)
+        insert_config = get_insert_config(current_node, symbolic_traced_module_dict, insert_mapping)
+        # If this node match the patter, a new node needs to be inserted after it
+        if insert_config.should_insert:
+            # Get the next original node
+            next_origin_node = current_node.next
+            # Create temporary pointer for inserting
+            with symbolic_traced_module_graph.inserting_after(current_node):
+                # Create new node after current node
+                new_node = symbolic_traced_module_graph.call_function(insert_config.function_package.function,
+                                                                      kwargs=insert_config.function_package.parameter_dict)
+                # Set the input of the new node to the output of the current node
+                set_node_input(new_node, get_node_output(current_node))
+                # Get the output of the new node
+                new_node_output = get_node_output(new_node)
+                # Link the output of the new node to the input of the next original node
+                set_node_input(next_origin_node, new_node_output)
 
     symbolic_traced_module_graph.lint()
     return torch.fx.GraphModule(model_input, symbolic_traced_module_graph)
